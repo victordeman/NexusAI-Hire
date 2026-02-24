cd9c71a1487225e440add28ef99a4c79e95d1087
Dr.-Ing. Chukwuka Victor obionwu <victordeman@users.noreply.github.com>
2026-02-24 02:40:53 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: 'frontend',
+  base: './',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'frontend/index.html'),
+        interview: resolve(__dirname, 'frontend/interview.html'),
+        dashboard: resolve(__dirname, 'frontend/dashboard.html'),
+      },
+    },
+  },
+});
